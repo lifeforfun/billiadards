066d1a8cb4573d4d51f98ba0f791ef2f2147cca9
pc <home@local.com>
2017-08-24 19:11:42 +0800

    Signed-off-by: pc <home@local.com>

--- environments/prod/common/config/params-local.php
+++ environments/prod/common/config/params-local.php
@@ -2,12 +2,12 @@
 return [
     'emchat' => [
         // 企业id
-        'org' => '1182170609115040',
+        'org' => '1138170818115604',
         // 应用名
         'app' => 'billiards',
         // appkey: org_name#app_name
-        'appkey' => '1182170609115040#billiards',
-        'client_id' => 'YXA6s1VssEyuEeeYGkflyM9OMg',
-        'client_secret' => 'YXA6EVPPRAn7r6pncR75GcZ6BPVvo_s',
+        'appkey' => '1138170818115604#billiards',
+        'client_id' => 'YXA6xomFYIi8EeefGo3st1v_Lw',
+        'client_secret' => 'YXA6sTTr_vot-ID5kH4ojfHkYm_gxRA',
     ]
 ];
