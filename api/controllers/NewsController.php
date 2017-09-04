a3ebb2e323211c5b76c884ed20924f3bb7aee88a
pc <home@local.com>
2017-09-04 20:23:21 +0800

    Signed-off-by: pc <home@local.com>

--- api/controllers/NewsController.php
+++ api/controllers/NewsController.php
@@ -31,7 +31,7 @@ class NewsController extends \api\lib\Controller
 
         $query->filterWhere([
             'status' => 1,
-            'q' => $q,
+            'title' => $q,
         ]);
 
         if ($tags!=='') {
